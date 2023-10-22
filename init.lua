e3bbc0090dd0303be3eb37f5295e0f3004d0c0ab
adsec33 <adsec33@gmail.com>
2023-10-22 21:29:26 +0800

    LSP Mason added ui config from init.lua with custom path installed

--- init.lua
+++ init.lua
@@ -141,4 +141,14 @@ require("nvim-tree").setup {
 }
 
 
+ require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    },
+    install_root_dir = "./mason",
 
+})
