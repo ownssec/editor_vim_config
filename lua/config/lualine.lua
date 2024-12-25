94cf5711776b498cd96dd0e0caf1463cbc8ee1df
ownssec <ownssec@gmail.com>
2024-12-25 21:24:55 +0800

    added lua line

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -6,43 +6,34 @@ if not status then
 end
 
 lualine.setup({
-    options = {
-        icons_enabled = true,
-        theme = "powerline",
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-        disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = false,
-        refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-        },
+  options = {
+    icons_enabled = false,
+    theme = bubbles_theme,
+    component_separators = ' | ',
+    section_separators = { left = ' | ', right = ' | ' },
+  },
+  sections = {
+    lualine_a = {
+      { 'mode' },
     },
-    sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { "filename" },
-        lualine_x = { "encoding", "fileformat", "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
+    lualine_b = {'filename', 'branch',  'location'},
+    lualine_c = { 'filetype' },
+    lualine_x = {''},
+    lualine_y = { 'filetype' },
+    lualine_z = {
+      { 'diff', 'diagnostics',separator = { right = '' }, left_padding = 2 },
     },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {},
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {},
-})
+  },
+  inactive_sections = {
+    lualine_a = { '' },
+    lualine_b = {''},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = { '' },
+  },
+  tabline = {},
+  extensions = {},
+}
+)
 
