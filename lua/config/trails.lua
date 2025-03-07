09d449e0bd7ed1dff2d158d4602361c7bd302114
ownssec <ownssec@gmail.com>
2025-03-08 00:22:16 +0800

    trailer hotkey for marking update and also the conform update

--- lua/config/trails.lua
+++ lua/config/trails.lua
@@ -6,7 +6,7 @@ require("trailblazer").setup({
 	mappings = { -- rename this to "force_mappings" to completely override default mappings and not merge with them
 		nv = { -- Mode union: normal & visual mode. Can be extended by adding i, x, ...
 			motions = {
-				new_trail_mark = "ma",
+				new_trail_mark = "m",
 				-- track_back = "<A-b>",
 				peek_move_next_down = "]t",
 				peek_move_previous_up = "[t",
