96c69f0ed21992ae5fcf9fcd944c5ea2455604ba
ownssec <ownssec@gmail.com>
2025-08-10 04:29:00 +0800

    update

--- lua/config/wilder.lua
+++ lua/config/wilder.lua
@@ -1,5 +1,9 @@
 -- lua/configs/wilder.lua
 local wilder = require("wilder")
+
+local width = "100%"
+local height = "20%"
+
 wilder.setup({
 	modes = { ":", "/", "?" },
 	next_key = "<C-n>",
@@ -56,27 +60,34 @@ wilder.set_option("pipeline", {
 				"--glob",
 				"!**/.git/*",
 			},
-		})
+		}),
+
+		wilder.set_option(
+			"renderer",
+			wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+				left = { " ", wilder.popupmenu_devicons() },
+				right = { " ", wilder.popupmenu_scrollbar() },
+				pumblend = 0,
+				border = { " ", " ", " ", " ~ ", " ", "", "", " " },
+				highlights = { border = "WilderBorder" },
+				max_width = width,
+				min_width = width,
+				max_height = height,
+				min_height = height,
+				reverse = 0,
+			}))
+
+			-- wilder.popupmenu_renderer(wilder.popupmenu_palette_theme({
+			-- 	-- 'single', 'double', 'rounded' or 'solid'
+			-- 	-- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
+			-- 	border = "rounded",
+			-- 	max_height = "75%", -- max height of the palette
+			-- 	min_height = 0, -- set to the same as 'max_height' for a fixed height window
+			-- 	prompt_position = "top", -- 'top' or 'bottom' to set the location of the prompt
+			-- 	reverse = 0, -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+			-- }))
+		)
 	),
 })
-
-local width = "100%"
-local height = "20%"
-
-wilder.set_option(
-	"renderer",
-	wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
-		left = { " ", wilder.popupmenu_devicons() },
-		right = { " ", wilder.popupmenu_scrollbar() },
-		pumblend = 0,
-		border = { " ", " ", " ", " ~ ", " ", "", "", " " },
-		highlights = { border = "WilderBorder" },
-		max_width = width,
-		min_width = width,
-		max_height = height,
-		min_height = height,
-		reverse = 0,
-	}))
-)
 
 vim.api.nvim_set_hl(0, "WilderBorder", { fg = "#414243" }) -- border color
